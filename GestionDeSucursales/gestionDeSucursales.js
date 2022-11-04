b98604025a2fdf4adb8ec71a0911993fe83f0790
Luciano Frangolini <64044827+LucianoFrangolini@users.noreply.github.com>
2022-11-04 18:47:10 -0300

    Cambios del 31/10

--- GestionDeSucursales/gestionDeSucursales.js
+++ GestionDeSucursales/gestionDeSucursales.js
@@ -1,70 +1,51 @@
 const sucursales = require('./sucursales.json');
-
-console.log(sucursales)
-
-const http = require('http')
+const http = require('http');
+const config = require('./config.json');
 
 //busca sucursal por id, se supone que el id existe, en caso de no existir devuelve null
-var buscaSucursal = function(id){
-  for(let i=0; i<sucursales.length; i++){
-    if(sucursales[i].id == id){
+var buscaSucursal = function (id) {
+  for (let i = 0; i < sucursales.length; i++) {
+    if (sucursales[i].id == id) {
       return sucursales[i];
     }
   }
-  return null
+  return null;
 }
 
-var myVar = (request, response) => {
-  let url = request.url.substring(1);
-  let method = request.method;
-  console.log(url)
-  console.log(method)
-  console.log('2) socket connected')
-  response.setHeader('Content-Type', 'application/json')
-  let res ={}
-  url = url.split('/');
-  if(method == 'GET'){
-    if(url[0]!= 'api' || url[1] != 'sucursales'){
-      response.writeHead(400)
-      res={
-        messageError: 'Servicio no encontrado'
-      }
-    }else{
-      if(url.length == 2){ //devolver todas las sucursales
-        response.writeHead(200)
-        res = sucursales
-      }else{ //devolver sucursal de id especifica
-        let suc = buscaSucursal(url[2]);
-        console.log(suc)
-        if(suc != null){
+const server = http.createServer( (request, response) => {
+  let res;
+  let url = request.url.split("/").filter(Boolean);
+  response.setHeader('Content-Type', 'application/json');
+  if ((request.method === "GET") && (url[0] === "api") && (url[1] === "sucursales") && (url.length<=3)) {
+    if (url.length == 2) { //Devolver todas las sucursales
+      response.writeHead(successCode);
+      res = sucursales;
+    } else { //Devolver sucursal de id especifica
+      let suc = buscaSucursal(url[2]);
+      if (suc != null) {
         res = {
           'lat': suc.lat,
           'lng': suc.lng,
           'name': suc.name
         }
-        response.writeHead(200)
-      }else{
-        //la sucursal no fue encontrada
-        response.writeHead(400)
+        response.writeHead(successCode);
+      } else { //Error: La sucursal no fue encontrada
+        response.writeHead(sucursalNotFoundError)
         res = {
           messageError: 'Sucursal no encontrada'
         }
       }
     }
-  }
-  }else{ //codigo de error con mensaje 
-    response.writeHead(400)
+  } else { //Error: El servicio no existe
+    response.writeHead(serviceNotFoundError)
     res = {
       messageError: 'Servicio no encontrado'
     }
   }
+  response.end(JSON.stringify(res));
+});
 
-      console.log(res)
-      response.end(JSON.stringify(res))
-}
 
-const server = http.createServer(myVar);
-
-server.listen(8080, function() {
-  console.log('1) Server started');
+server.listen(port, function () {
+  console.log(`Server started on port: ${port}`);
 });
